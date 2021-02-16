23fd4ddf52c040a3e6592848405cf56af4d9ce70
hong yuseok <jericoh@naver.com>
2021-02-16 21:33:43 +0900

    edit baseurl

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,8 @@
 const {createProxyMiddleware} = require('http-proxy-middleware')
 
-const baseURL = 'http://localhost:8004'
+// const baseURL = 'http://localhost:8004'
+// const baseURL = 'http://localhost:80'
+const baseURL = 'http://18.191.99.176/'
 
 module.exports = (app) => {
   app.use(
