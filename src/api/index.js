d0d419a835c7592d406adf04d8784acd9e56b346
hong yuseok <jericoh@naver.com>
2021-03-08 01:48:36 +0900

    api index 수정

--- src/api/index.js
+++ src/api/index.js
@@ -11,23 +11,23 @@ async function errorHandler(e) {
   throw e
 }
 
-if (process.env.NODE_ENV !== 'production') {
-  instance.interceptors.request.use(
-    (config) => {
-      const {url, method, params, data} = config
-      // eslint-disable-next-line no-console
-      console.log(`[Request]: ${JSON.stringify({url, params, method, data}, null, 2)}`)
-      return config
-    },
-    (error) => Promise.reject(error)
-  )
-  instance.interceptors.response.use((response) => {
-    // eslint-disable-next-line no-console
-    console.log(`[Response Data]: ${JSON.stringify(response.data, null, 2)}`)
-    return response
-  }, errorHandler)
-} else {
-  instance.interceptors.response.use((response) => response, errorHandler)
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   instance.interceptors.request.use(
+//     (config) => {
+//       const {url, method, params, data} = config
+//       // eslint-disable-next-line no-console
+//       console.log(`[Request]: ${JSON.stringify({url, params, method, data}, null, 2)}`)
+//       return config
+//     },
+//     (error) => Promise.reject(error)
+//   )
+//   instance.interceptors.response.use((response) => {
+//     // eslint-disable-next-line no-console
+//     console.log(`[Response Data]: ${JSON.stringify(response.data, null, 2)}`)
+//     return response
+//   }, errorHandler)
+// } else {
+//   instance.interceptors.response.use((response) => response, errorHandler)
+// }
 
 export default instance
