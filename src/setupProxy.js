e4f382eb5ebb6b5fa0ee2f0d604f290e6732ef9c
hong yuseok <jericoh@naver.com>
2021-03-08 02:05:21 +0900

    setupproxy 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,13 +2,13 @@ const {createProxyMiddleware} = require('http-proxy-middleware')
 
 // const baseURL = 'http://localhost:8004'
 // const baseURL = 'http://localhost:80'
-const baseURL = 'http://www.simplecrud.tk/'
+// const baseURL = 'http://www.simplecrud.tk/'
 
 module.exports = (app) => {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: baseURL,
+      target: 'http://www.simplecrud.tk/',
       changeOrigin: true
     })
   )
